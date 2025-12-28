420a9fb4b44da9cfe07c36abce07e0dc23b06e0f
monia <23l31a05p7@gmail.com>
2025-12-28 20:45:27 +0530

    Deleted data folder and added it to scripts

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,6 @@
-    const productsList=[{
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars:4.5,
-            count: 87,
-        },
-        price: 1090,
-    },
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars:4,
-            count: 127,
-        },
-        price: 2095,
-    },
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars:4.5,
-            count: 56,
-        },
-        price: 799,
-    }
-];
-
-let products='';
-productsList.forEach((value)=>{
-    products+=`
+let productsHTML='';
+products.forEach((value)=>{
+    productsHTML+=`
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image" src=${value.image}>
@@ -48,9 +19,9 @@ productsList.forEach((value)=>{
           </div>
 
           <div class="product-price">
-            $${(value.price / 100).toFixed(2)}
+            $${(value.priceCents / 100).toFixed(2)}
           </div>
         </div>`
 });
 
-document.querySelector('.products-grid').innerHTML=products;
+document.querySelector('.products-grid').innerHTML=productsHTML;
