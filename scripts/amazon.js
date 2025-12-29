f40c037dc260a99b4a9197fa32c9776caeb2a5c3
monia <23l31a05p7@gmail.com>
2025-12-29 19:49:30 +0530

    Made add-to-cart btn interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,7 +21,52 @@ products.forEach((value)=>{
           <div class="product-price">
             $${(value.priceCents / 100).toFixed(2)}
           </div>
+         <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-button-to-cart" data-product-id="${value.id}">
+            Add to Cart
+          </button>
         </div>`
 });
 
 document.querySelector('.products-grid').innerHTML=productsHTML;
+
+document.querySelectorAll(".js-button-to-cart").forEach((button)=>{
+  button.addEventListener('click',()=>{
+    const productId= button.dataset.productId;
+    
+    let itemFound;
+    cart.forEach((item)=>{
+      if(productId===item.productId) itemFound=item;
+    });
+
+    if(itemFound) itemFound.quantity+=1;
+    else{
+      cart.push({
+      productId: productId,
+      quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
