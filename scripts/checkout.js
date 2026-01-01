29bdd14ae2674838f87ebad1d2d3495ab4981a9a
monia <moniavoona18@gmail.com>
2026-01-01 17:26:28 +0530

    Fixed errors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
-import {cart, removeFromCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {cart, removeFromCart} from './cart.js';
+import {products} from './products.js';
 import {formatCurrency} from './utils/money.js';
 
 let cartSummaryHTML = '';
